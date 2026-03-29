a7ce27423143cc8f7776afaa6eb0913457e277fb
iolindt <ramazzanov@gmail.com>
2026-03-29 15:11:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Hello! This is a sample project: Text Analyzer")
+print("1 - Count words")
+print("2 - Uppercase")
+choice = input("Choose: ")
+
+text = input("Enter text: ")
+
+if choice == "1":
+    print("Words:", len(text.split()))
+elif choice == "2":
+    print(text.upper())
